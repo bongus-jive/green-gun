58e84802d81066b23364e0b1288f726c383f4459
patman <27874300+patmanf@users.noreply.github.com>
2022-01-16 17:17:00 -0600

    make Greened entities fullbrighted

--- pat/greengun/green.lua
+++ pat/greengun/green.lua
@@ -1,6 +1,6 @@
 function init()
 	if effect.sourceEntity() ~= entity.id() then
-		effect.setParentDirectives("setcolor="..config.getParameter("color", "0f0"))
+		effect.setParentDirectives("setcolor="..config.getParameter("color", "0f0").."?multiply=fffffffe")
 		script.setUpdateDelta(0)
 		effect.modifyDuration(math.huge)
 	else
